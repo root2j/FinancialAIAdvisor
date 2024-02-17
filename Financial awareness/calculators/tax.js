d01afb072d859d8b1f4237b27376f4294f06c4df
Rahulsolanke2007 <135037854+Rahulsolanke2007@users.noreply.github.com>
2024-02-17 20:44:38 +0530

    Add files via upload
    Calculators implemented

---
+++ Financial awareness/calculators/tax.js
@@ -0,0 +1,20 @@
+// script.js
+function calculateTax() {
+    const income = parseFloat(document.getElementById('income').value);
+  
+    let tax = 0;
+  
+    if (income <= 250000) {
+      tax = 0;
+    } else if (income > 250000 && income <= 500000) {
+      tax = (income - 250000) * 0.05;
+    } else if (income > 500000 && income <= 1000000) {
+      tax = 12500 + (income - 500000) * 0.2;
+    } else {
+      tax = 112500 + (income - 1000000) * 0.3;
+    }
+  
+    const output = document.getElementById('output');
+    output.innerHTML = `<p>Your Income Tax: ₹${tax.toFixed(2)}</p>`;
+  }
+  
